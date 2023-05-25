bbf718c35bbbd5e442c733c7d570aabe8fbc21d5
Subhan Raj <shubhanraj2002@gmail.com>
2023-05-25 12:02:21 +0530

    Sumn

--- User-Input/input.py
+++ User-Input/input.py
@@ -10,4 +10,12 @@ future_age = input("After how many years do you want to know your age? ")
 new_age = int(future_age) + int(current_age) # Convert string to integer
 print("You'll be " + str(new_age) + " years old after " + future_age + " years.") # Convert integer to string
 
+# Printing sum of two numbers
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+sum = float(num1) + float(num2) # Convert string to float
+print("The sum of {0} and {1} is {2}".format(num1, num2, sum)) # Format method
+
 
